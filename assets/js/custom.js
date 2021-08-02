49a4f43f21bf2fb6ebd9080f1f021c74fd0e6b5f
Serhii Pavlovych <programmersp@gmail.com>
2021-08-02 17:28:18 +0300

    Remove scroll to top

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -18,22 +18,9 @@ $(document).ready(function(){
 ======================================*/
 
     // 1. Scroll To Top 
-		$(window).on('scroll',function () {
-			if ($(this).scrollTop() > 600) {
-				$('.return-to-top').fadeIn();
-			} else {
-				$('.return-to-top').fadeOut();
-			}
-		});
-		$('.return-to-top').on('click',function(){
-				$('html, body').animate({
-				scrollTop: 0
-			}, 1500);
-			return false;
-		});
+
 	
-	
-	// 2. owl carousel
+    // 2. owl carousel
 	
 		// i. client (carousel)
 		
